3b5f32463671ec97bff8040f96c40c7bd3141da6
Gerson Arbigaus <gerson87@gmail.com>
2019-04-14 21:42:23 -0300

    started layout from pet details

--- adocaoAnimal/adocaoAnimal/Modules/Pet/PetDetailsView.swift
+++ adocaoAnimal/adocaoAnimal/Modules/Pet/PetDetailsView.swift
@@ -16,14 +16,31 @@ protocol PetDetailsViewDelegate: class {
 
 class PetDetailsView: UIViewController {
     
+    @IBOutlet weak var petImage: UIImageView!
+    @IBOutlet weak var ownerImage: UIImageView!
+    @IBOutlet weak var petNameLabel: UILabel!
+    @IBOutlet weak var petDescriptionLabel: UILabel!
+    @IBOutlet weak var ownerName: UILabel!
+    @IBOutlet weak var petDescriptionText: UITextView!
+    
+    @IBOutlet weak var petDetailsCollection: UICollectionView!
+    
+    @IBOutlet weak var adoptButtom: UIButton!
+    
     var viewModel: PetDetailsViewModel!
     weak var delegate: AppActionable?
     
-    var pet: Pet?
+    var pet: Pet
 
     init(pet: Pet) {
         self.pet = pet
         super.init(nibName: String(describing: PetDetailsView.self), bundle: nil)
+        
+        self.petImage?.image = UIImage(named: self.pet.image)
+        self.petNameLabel?.text = self.pet.name
+        self.ownerName?.text = self.pet.owner
+        
+//        adoptButtom.layer.borderColor = UIColor(red:255/255, green:255/255, blue:255/255, alpha: 0.8).cgColor
         
     }
     
